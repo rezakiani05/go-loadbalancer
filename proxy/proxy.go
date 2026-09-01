cafeb51322616466ef7866f6fd05d52a79910070
Reza <rezakiani84.ma@gmail.com>
2026-09-01 15:52:08 +0330

    complete proxy

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,9 +1,12 @@
 package proxy
 
 import (
+	"net"
+	"net/http"
+	"net/http/httputil"
+
 	"go-loadbalancer/balancer"
 	"go-loadbalancer/ratelimiter"
-	"net/http"
 )
 
 type GatewayHandler struct {
@@ -12,16 +15,30 @@ type GatewayHandler struct {
 }
 
 func NewGatewayHandler(lb *balancer.LoadBalancer, rl *ratelimiter.RateLimiter) *GatewayHandler {
-	// TODO: مقداردهی ساختار GatewayHandler.
-	return nil
+	return &GatewayHandler{
+		lb:          lb,
+		rateLimiter: rl,
+	}
 }
 
 func (g *GatewayHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// TODO:
-	// ۱. استخراج IP کاربر از r.RemoteAddr.
-	// ۲. چک کردن با g.rateLimiter.Allow(ip). اگر false بود:
-	//    http.Error(w, "Too Many Requests", http.StatusTooManyRequests) بده و return کن.
-	// ۳. گرفتن سرور بعدی با backend := g.lb.GetNextValidBackend().
-	//    اگر backend nil بود: http.Error(w, "Service Unavailable", http.StatusServiceUnavailable) بده.
-	// ۴. ساخت یک ReverseProxy با httputil.NewSingleHostReverseProxy(backend.URL) و اجرای ServeHTTP(w, r) روی آن.
+
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		host = r.RemoteAddr
+	}
+
+	if !g.rateLimiter.Allow(host) {
+		http.Error(w, "Too Many Requests", http.StatusTooManyRequests)
+		return
+	}
+
+	backend := g.lb.GetNextValidBackend()
+	if backend == nil {
+		http.Error(w, "Service Unavailable", http.StatusServiceUnavailable)
+		return
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(backend.URL)
+	proxy.ServeHTTP(w, r)
 }
